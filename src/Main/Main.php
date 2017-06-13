84052eaf23a2931346acf52d67c70c541ba89db9
den <leontii33@gmail.com>
2017-06-13 06:39:16 +0300

    Обновление

--- src/Main/Main.php
+++ src/Main/Main.php
@@ -9,27 +9,26 @@
   use pocketmine\event\entity\EntityDamageByEntityEvent;
   use pocketmine\event\entity\EntityExplodeEvent;
   
-    class Main extends PluginBase implements Listener{
-        public function onEnable(){
-             $this->getLogger()->info("Плагин успешно загружен!");
-             $this->getServer()->getPluginManager()->registerEvents($this, $this);
-          }
-        public function antiBoom(EntityExplodeEvent $e){
-              $e->setCancelled(true);
-        }
+	class Main extends PluginBase implements Listener{
+		public function onEnable(){
+			$this->getLogger()->info("Плагин успешно загружен!");
+			$this->getServer()->getPluginManager()->registerEvents($this, $this);
+		}
+		public function antiBoom(EntityExplodeEvent $e){
+			$e->setCancelled(true);
+		}
         
-        public function antiDmg(EntityDamageEvent $e){
-             $en = $e->getEntity();
-             $dmg = $e->getDamager();
-               if($dmg instanceof Player && $en instanceof Player){
-              	if($dmg->hasPermission("antigm")){
-                     if($dmg->getGameMode() == 1){
-                        $e->setCancelled(true);
-                    }elseif(
-                       $dmg->isFlying() != null){
-                    }
-               }
-         }
-        }
-      
+		public function antiDmg(EntityDamageEvent $e){
+			$en = $e->getEntity();
+			$dmg = $e->getDamager();
+				if($dmg instanceof Player && $en instanceof Player){
+				if($dmg->hasPermission("antigm")){
+				if($dmg->getGameMode() == 1){
+					$e->setCancelled(true);
+				}elseif(
+					$dmg->isFlying() != null){
+				}
+			}
+		}
+	}
 }
